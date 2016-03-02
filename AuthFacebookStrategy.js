32841e08eca82fee899f11d6824a760b25ad1509
ridakk <kadirgoktas@gmail.com>
2016-03-02 15:56:49 +0200

    prod auth strategy update

--- AuthFacebookStrategy.js
+++ AuthFacebookStrategy.js
@@ -7,9 +7,9 @@ var logger = require('bunyan').createLogger({
   FacebookStrategy = require('passport-facebook').Strategy;
 
 passport.use(new FacebookStrategy({
-    clientID: "---",
-    clientSecret: "---",
-    callbackURL: "---",
+  clientID: process.env.FACEBOOK_CLIENT_ID,
+  clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  callbackURL: process.env.FACEBOOK_CLIENT_CALLBACKURL,
     profileFields: ['id', 'displayName', 'photos'],
     passReqToCallback: true
   },
