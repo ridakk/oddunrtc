b3aede86c41705653438355880e12505ef5f58f2
ridakk <kadirgoktas@gmail.com>
2016-03-21 23:08:48 +0200

    demo setup fix

--- public/main/js/widget/widget.js
+++ public/main/js/widget/widget.js
@@ -17,7 +17,7 @@
   document.getElementById("odun-rtc-video-widget").appendChild(button);
 
   document.getElementById("odun-rtc-video-widget-button").addEventListener("click", function() {
-    window.open('http://young-tundra-62254.herokuapp.com/a/c2c_d4b1dd30-eed1-11e5-9790-d3dd181bee66', '_blank', 'width=640,height=480');
+    window.open('https://young-tundra-62254.herokuapp.com/a/c2c_d4b1dd30-eed1-11e5-9790-d3dd181bee66', '_blank', 'width=640,height=480');
   });
 
 })(window, document);
