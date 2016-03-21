b1440fd76150c6f901e181c1fd71a2c21448e1dc
ridakk <kadirgoktas@gmail.com>
2016-03-21 23:05:45 +0200

    demo setup

--- public/main/js/widget/widget.js
+++ public/main/js/widget/widget.js
@@ -2,7 +2,7 @@
   var button = document.createElement('button');
   button.type = "submit";
   button.id = "odun-rtc-video-widget-button";
-  button.innerHTML = 'Need help !!!';
+  button.innerHTML = 'Bizi Arayın!!!';
   button.style.cssText = "width: %100;" +
     "height: 30px;" +
     "border: 1px solid transparent;" +
@@ -17,7 +17,7 @@
   document.getElementById("odun-rtc-video-widget").appendChild(button);
 
   document.getElementById("odun-rtc-video-widget-button").addEventListener("click", function() {
-    window.open('http://localhost:5000/a/c2c_bc7ba9a0-dec6-11e5-8bac-753c65bebdb9', '_blank', 'width=640,height=480');
+    window.open('http://young-tundra-62254.herokuapp.com/a/c2c_d4b1dd30-eed1-11e5-9790-d3dd181bee66', '_blank', 'width=640,height=480');
   });
 
 })(window, document);
