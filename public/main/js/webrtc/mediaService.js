cb3766f763cd74281d5234b645a07264c89f2924
ridakk <kadirgoktas@gmail.com>
2016-03-22 13:02:06 +0200

    KU demo fix

--- public/main/js/webrtc/mediaService.js
+++ public/main/js/webrtc/mediaService.js
@@ -7,27 +7,30 @@ angular.module('webrtc.mediaService', ['util.pubsub'])
         subscriber: pubsubSubscriber.media_service,
         event: pubsubMessage.request_media_permission,
         callback: function(data) {
-          navigator.getUserMedia(data.msg.constraints, function(stream) {
-            pubsub.publish({
-              publisher: pubsubSubscriber.media_service,
-              subscriber: pubsubSubscriber.call_fsm,
-              event: pubsubMessage.media_permission_granted,
-              msg: {
-                stream: stream,
-                callId: data.msg.callId
-              }
+          // KU DEMO FIX
+          setTimeout(function(){
+            navigator.getUserMedia(data.msg.constraints, function(stream) {
+              pubsub.publish({
+                publisher: pubsubSubscriber.media_service,
+                subscriber: pubsubSubscriber.call_fsm,
+                event: pubsubMessage.media_permission_granted,
+                msg: {
+                  stream: stream,
+                  callId: data.msg.callId
+                }
+              });
+            }, function(error) {
+              pubsub.publish({
+                publisher: pubsubSubscriber.media_service,
+                subscriber: pubsubSubscriber.call_fsm,
+                event: pubsubMessage.media_permission_rejected,
+                msg: {
+                  error: error,
+                  callId: data.msg.callId
+                }
+              });
             });
-          }, function(error) {
-            pubsub.publish({
-              publisher: pubsubSubscriber.media_service,
-              subscriber: pubsubSubscriber.call_fsm,
-              event: pubsubMessage.media_permission_rejected,
-              msg: {
-                error: error,
-                callId: data.msg.callId
-              }
-            });
-          });
+          }, 300);
         }
       });
     }
