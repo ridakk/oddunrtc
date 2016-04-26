26c64534c79b37fcb22c17a0a8e1e456198a69ed
ridakk <kadirgoktas@gmail.com>
2016-04-26 07:41:24 +0300

    oddjs webpack config

---
+++ oddjs/src/odd.js
@@ -0,0 +1,8 @@
+export default class OddJS {
+  constructor() {
+    this._name = 'OddJS';
+  }
+  get name() {
+    return this._name;
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack');
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var path = require('path');
+var env = require('yargs').argv.mode;
+
+var libraryName = 'odd';
+
+var plugins = [], outputFile;
+
+if (env === 'build') {
+  plugins.push(new UglifyJsPlugin({ minimize: true }));
+  outputFile = libraryName + '.min.js';
+} else {
+  outputFile = libraryName + '.js';
+}
+
+var config = {
+  entry: __dirname + '/oddjs/src/odd.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/oddjs/dist',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: "eslint-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    root: path.resolve('./oddjs/src'),
+    extensions: ['', '.js']
+  },
+  plugins: plugins
+};
+
+module.exports = config;
